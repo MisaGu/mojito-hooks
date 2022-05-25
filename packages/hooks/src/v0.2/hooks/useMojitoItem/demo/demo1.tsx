587a81befdf32424f469f33ee9ef83cdab01401c
Dani Gámez <gmzcodes@outlook.com>
2022-05-25 12:58:19 +0200

    Adds more demos.

--- packages/hooks/src/v0.2/hooks/useMojitoItem/demo/demo1.tsx
+++ packages/hooks/src/v0.2/hooks/useMojitoItem/demo/demo1.tsx
@@ -1,6 +1,27 @@
+/**
+ * title: Basic usage
+ * desc: This hook takes a `collectionItemID` param or a slug or pathname param (defaults to location.pathname).
+ */
+
 import React from 'react';
-// import { useMojitoItem } from 'hooks';
+import { DEMO_SLUG } from '../../../components/demo/constants/demo.constants';
+import { DemoInterface } from '../../../components/demo/Interface/DemoInterface';
+import { Json } from '../../../components/demo/Json/Json';
+import { DemoProviders } from '../../../components/demo/Provider/DemoProvider';
+import { useMojitoItem } from '../useMojitoItem';
+
+const DemoContent: React.FC = () => {
+  const result = useMojitoItem({
+    slug: DEMO_SLUG,
+  });
+
+  return <Json result={result} />;
+};
 
 export default () => {
-  return <div />;
+  return (
+    <DemoProviders>
+      <DemoInterface demoComponent={DemoContent} />
+    </DemoProviders>
+  );
 };
--- packages/hooks/src/v0.2/hooks/useMojitoWallets/demo/demo1.tsx
+++ packages/hooks/src/v0.2/hooks/useMojitoWallets/demo/demo1.tsx
@@ -1,6 +1,24 @@
+/**
+ * title: Basic usage
+ * desc: This hooks takes no props.
+ */
+
 import React from 'react';
-// import { useMojitoWallets } from 'hooks';
+import { DemoInterface } from '../../../components/demo/Interface/DemoInterface';
+import { Json } from '../../../components/demo/Json/Json';
+import { DemoProviders } from '../../../components/demo/Provider/DemoProvider';
+import { useMojitoWallets } from '../useMojitoWallets';
+
+const DemoContent: React.FC = () => {
+  const result = useMojitoWallets();
+
+  return <Json result={result} />;
+};
 
 export default () => {
-  return <div />;
+  return (
+    <DemoProviders>
+      <DemoInterface demoComponent={DemoContent} />
+    </DemoProviders>
+  );
 };
